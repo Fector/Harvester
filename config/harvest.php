4cc347f37b53b301e1c1d284017dc1f2845c6398
Mostovoy Evgeniy <emostovoi@phoenixit.ru>
2018-08-08 18:04:15 +0300

    add select decorator

--- config/harvest.php
+++ config/harvest.php
@@ -6,6 +6,7 @@ return [
         '_with' => \Fector\Harvest\Decorators\AssociationDecorator::class,
         '_with_cnt' => \Fector\Harvest\Decorators\CountDecorator::class,
         '_filter' => \Fector\Harvest\Decorators\FilterDecorator::class,
+        '_select' => \Fector\Harvest\Decorators\SelectDecorator::class,
     ],
     'delimiter' => ':',
     'subCommandDelimiter' => '->'
